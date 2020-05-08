92192de73d27fcb35c1c11c5b5b4b0197f8961e4
danish45007 <myemail>
2020-05-08 22:15:35 +0530

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+// Import's
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Exporting Routes
+const indexRoute = require('./Routes/index');
+const urlRoute = require('./Routes/url');
+
+
+// middleware that allows to accept json data to Api
+app.use(express.json({extented: false}));
+
+// Using Routes
+app.use("/",indexRoute);
+app.use("/api/url",urlRoute);
+
+
+// DB Connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED");
+})
+
+
+
+PORT = 5000;
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
